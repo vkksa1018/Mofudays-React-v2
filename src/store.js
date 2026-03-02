250ed8c1af098afd27a8a94c4b40b86f4d02906e
james <ohbwgab18@gmail.com>
2026-03-02 16:38:19 +0800

    3/2 userAuthSlice SET、router.jsx ESLint fix

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+//前台管理系統
+import userAuthReducer from "./slices/userAuthSlice";
+
 // 後台管理系統
 import adminAuthReducer from "./slices/adminAuthSlice";
 
 export const store = configureStore({
-    reducer: {
-    adminAuth: adminAuthReducer, 
+  reducer: {
+    adminAuth: adminAuthReducer,
+    userAuth: userAuthReducer,
     // cart: cartReducer,
   },
-})
+});
