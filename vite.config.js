eb631b95802f28a15f07d23c2d47e273b8a5a712
james <ohbwgab18@gmail.com>
2026-04-10 10:42:43 +0800

    chore: migrate frontend deployment from GitHub Pages to Vercel

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mofudays-react/",
+  base: "/",
   css: {
     preprocessorOptions: {
       scss: {
